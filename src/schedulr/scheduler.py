00cd70084e4bbe9f4a77a8180aec674555c24bf1
Paul <pm@smartdive.com>
2022-03-10 16:21:57 +0700

    Add cron trigger, capture stderr if error

--- src/schedulr/scheduler.py
+++ src/schedulr/scheduler.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
+from apscheduler.triggers.cron import CronTrigger
 from attrs import define
 
 from .report import Report
@@ -18,47 +19,58 @@ class BacklogEntry:
 class Scheduler:
 
     def __init__(self):
-        self.reports = []
-        self.backend = BackgroundScheduler()#jobstores=jobstores)
-        self.backend.start()
-        self.listeners = []
+        self._reports = []
+        self._backend = BackgroundScheduler()#jobstores=jobstores)
+        self._backend.start()
+        self._listeners = []
+
+    def stop(self):
+        self._backend.shutdown()
 
     def add_listener(self, listr):
-        self.listeners.append(listr)
+        self._listeners.append(listr)
 
     def run_job(self, cmd):
         ret = ''
         ex = None
         start = datetime.now()
         try:
-            ret = sp.check_output(cmd, shell=True)
-            ret = ret.decode('utf-8')
+            proc = sp.run(cmd, shell=True, check=True, capture_output=True)
+            ret = proc.stdout.decode('utf-8')
         except sp.CalledProcessError as e:
+            ret = e.stderr.decode('utf-8')
             ex = e
         end = datetime.now()
         r = Report(cmd, ret, start, end, ex)
-        self.reports.append( r )
+        self._reports.append( r )
 
         try:
-            for l in self.listeners:
+            for l in self._listeners:
                 l.send(r)
         except Exception as e:
-            tb.print_exc()
+            #tb.print_exc()
+            print(f'Notification failed with error {e}')
             # fail fast
-            self.backend.stop()
-            exit(1)
+            
+            #self._backend.shutdown()
+            #exit(1)
 
     def add_job(self, cmd, sched=30):
         trg = IntervalTrigger(seconds=sched)
-        self.backend.add_job(self.run_job, trigger=trg, name=cmd, args=(cmd,) )
+        self._backend.add_job(self.run_job, trigger=trg, name=cmd, args=(cmd,) )
+
+    def add_cron_job(self, cmd, crontab:str):
+        trg = CronTrigger.from_crontab(crontab)
+        self._backend.add_job(self.run_job, trigger=trg, name=cmd, args=(cmd,) )
+        
 
     def get_recent_jobs(self, n:int) -> list:
-        reports = self.reports
+        reports = self._reports
         n = min(n, len(reports))
         return reports[-n:]
 
     def get_future_jobs(self, n:int) -> typing.List[BacklogEntry]:
-        jobs = self.backend.get_jobs()
+        jobs = self._backend.get_jobs()
         qq = queue.PriorityQueue()
         ret = []
         for j in jobs:
