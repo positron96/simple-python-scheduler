4f783d3f51e576969910c6b0118edd905818aaa3
Paul <pm@smartdive.com>
2022-03-10 16:22:25 +0700

    Use SMTP user/pass if present, change email date format

--- src/schedulr/email_sender.py
+++ src/schedulr/email_sender.py
@@ -15,16 +15,21 @@ class Emailer:
 
         msg = EmailMessage()
         msg.set_content( ('Command: "{0}"\n'
-            'Start time: {1:%a %b %d %H:%M:%S %Y}\n'
-            'Finish time: {2:%a %b %d %H:%M:%S %Y}\n'
-            'Output: {3}\n'
-            '{4}').format( rep.cmd, rep.start, rep.end, rep.ret, '' if rep.ex is None else str(rep.ex) ) )
+            'Start time: {1:%x %X}\n'
+            'Finish time: {2:%x %X}\n'
+            '{3}\n'
+            '{4}' ).format( rep.cmd, rep.start, rep.end, 
+                '' if rep.ex is None else str(rep.ex),
+                rep.ret ) )
 
         msg['Subject'] = 'Execution result of "{}"'.format(rep.cmd)
         msg['From'] = self._cfg.get('from', 'sender@example.com')
         msg['To'] = self._cfg.get('to', 'target@example.com')
 
         s = smtplib.SMTP(self._cfg.get('server', 'localhost'), 
-                port=self._cfg.getint('server_port',1025) )
+                port=self._cfg.getint('server_port', 1025) )
+        user,psw = self._cfg.get('server_username',''), self._cfg.get('server_password', '')
+        if user:
+            s.login(user, psw)
         s.send_message(msg)
         s.quit()
